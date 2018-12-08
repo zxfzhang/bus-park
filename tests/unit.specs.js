9c5113fbc97627fe909fc10b806bca2635e35388
zxfzhang <zxfzhang1981@gmail.com>
2018-12-08 19:00:47 +0800

    Refine file path within import

--- tests/unit.specs.js
+++ tests/unit.specs.js
@@ -1,9 +1,9 @@
-import CarParkSpec from '/tests/specs/unit/ops/carPark.spec';
-import CmdPatternSpec from '/tests/specs/unit/command/cmdPattern.spec';
-import CmdTranslatorSpec from '/tests/specs/unit/command/cmdTranslator.spec';
-import RobotSpec from '/tests/specs/unit/ops/robot.spec';
-import CmdReaderSpec from '/tests/specs/unit/command/cmdReader.spec';
-import CmdStoreSpec from '/tests/specs/unit/command/cmdStore.spec';
+import CarParkSpec from 'tests/specs/unit/ops/carPark.spec';
+import CmdPatternSpec from 'tests/specs/unit/command/cmdPattern.spec';
+import CmdTranslatorSpec from 'tests/specs/unit/command/cmdTranslator.spec';
+import RobotSpec from 'tests/specs/unit/ops/robot.spec';
+import CmdReaderSpec from 'tests/specs/unit/command/cmdReader.spec';
+import CmdStoreSpec from 'tests/specs/unit/command/cmdStore.spec';
 import AppSpec from 'tests/specs/unit/app.spec';
 
 describe('[bus-park unit test]', () => {
